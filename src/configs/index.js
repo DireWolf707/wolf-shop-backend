66b2c2bf0d1c4490895a9624108255e592230f02
DireWolf707 <rahulsood707@gmail.com>
2023-05-15 14:56:05 +0530

    integrated razorpay

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,5 +3,6 @@ import prisma from "./prisma"
 import redis from "./redis"
 import storage from "./storage"
 import "./passport"
+import razorpay from "./razorpay"
 
-export { storage, prisma, redis }
+export { storage, prisma, redis, razorpay }
