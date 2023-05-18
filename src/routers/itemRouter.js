5af939283174dd9dbc26f1ed94b1cdcc2d65745c
DireWolf707 <rahulsood707@gmail.com>
2023-05-18 12:40:14 +0530

    added orders route

--- src/routers/itemRouter.js
+++ src/routers/itemRouter.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getItemList, getItemDetail, checkout,orderWebhook } from "../controllers/itemController"
+import { getItemList, getItemDetail, checkout, orderWebhook, getOrders } from "../controllers/itemController"
 import { isAuthenticated } from "../middlewares/auth"
 
 const router = express.Router()
 router.get("/", getItemList)
+router.get("/orders", isAuthenticated, getOrders)
 router.post("/checkout", isAuthenticated, checkout)
 router.post("/order-webhook", orderWebhook)
 router.route("/:itemId").get(getItemDetail)
